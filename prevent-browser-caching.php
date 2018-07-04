aabbd785828aa121e8bf4aa2275e6fce68925a95
Kostya Tereshchuk <kostyatereshchuk@gmail.com>
2018-07-04 16:23:07 +0300

    Version 2.2

--- prevent-browser-caching.php
+++ prevent-browser-caching.php
@@ -1,205 +1,70 @@
 <?php
 /**
  * Plugin Name: Prevent Browser Caching
- * Description: Updates the version of all CSS and JS files.
- * Version: 2.0
+ * Description: Update the version of all CSS and JS files. Show the latest changes on the site without asking the client to clear browser cache.
+ * Version: 2.2
  * Author: Kostya Tereshchuk
  * Author URI: https://wordpress.org/support/users/kostyatereshchuk/
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: prevent-browser-caching
+ * Domain Path: /lang/
  */
-
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-
-if ( ! class_exists('Prevent_Browser_Caching') ) {
-
-    /**
-     * @class Prevent_Browser_Caching
-     */
-    class Prevent_Browser_Caching
-    {
-
-        /**
-         * Single instance of the class.
-         *
-         * @var Prevent_Browser_Caching
-         */
-        protected static $_instance = null;
-
-        /**
-         * Value of prevent_browser_caching_options option.
-         *
-         * @var array
-         */
-        public $options = array();
-
-        /** Value of prevent_browser_caching_clear_cache_time option.
-         *
-         * @var string
-         */
-        public $clear_cache_time = '';
-
-        /** Url parameter "time" which will be added to styles and scripts.
-         *
-         * @var string
-         */
-        public $time_query_arg = '';
-
-
-        /**
-         * Prevent_Browser_Caching instance.
-         *
-         * @static
-         * @return Prevent_Browser_Caching - Main instance
-         */
-        public static function instance()
-        {
-            if (is_null(self::$_instance)) {
-                self::$_instance = new self();
-            }
-            return self::$_instance;
-        }
-
-        /**
-         * Prevent_Browser_Caching Constructor.
-         */
-        public function __construct()
-        {
-            $options = $this->get_options();
-
-            $clear_cache_automatically = $options['clear_cache_automatically'];
-
-            $time = '';
-            if ( $clear_cache_automatically == 'every_time' ) {
-                $time = time();
-            } elseif ( $clear_cache_automatically == 'every_period' ) {
-                $update_time = true;
-
-                if ( isset( $_COOKIE['prevent_browser_caching_time'] ) ) {
-                    $time = intval( $_COOKIE['prevent_browser_caching_time'] );
-                    $time = max( $time, $this->get_clear_cache_time() );
-                    $current_time = time();
-                    $cached_minutes = round( ( $current_time - $time ) / 60 );
-                    $options['clear_cache_automatically_minutes'];
-
-                    if ( $cached_minutes > $options['clear_cache_automatically_minutes'] ) {
-                        $update_time = true;
-                    } else {
-                        $update_time = false;
-                    }
-                }
-
-                if ( $update_time ) {
-                    $time = time();
-                    $expiration_time = $time + 60 * $options['clear_cache_automatically_minutes'];
-                    setcookie( 'prevent_browser_caching_time', $time, $expiration_time, '/' );
-                }
-            } elseif ( $clear_cache_automatically == 'never' ) {
-                $time = $this->get_clear_cache_time();
-            }
-
-            $this->time_query_arg = $time;
-
-            add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), array( $this, 'plugin_actions' ), 10, 1 );
-
-            add_action( 'plugins_loaded', array( $this, 'load_textdomain' ) );
-
-            add_filter( 'style_loader_src', array( $this, 'add_query_arg' ), 10000 );
-            add_filter( 'script_loader_src', array( $this, 'add_query_arg' ), 10000 );
-
-            if ( is_admin() ) {
-                include_once 'includes/admin/class-prevent-browser-caching-admin-settings.php';
-            }
-        }
-
-        /**
-         * Add settings to plugin links.
-         * @param $actions
-         * @return mixed
-         */
-        public function plugin_actions($actions) {
-            array_unshift( $actions, "<a href=\"" . menu_page_url( 'prevent-browser-caching', false ) . "\">" . esc_html__( "Settings" ) . "</a>" );
-            return $actions;
-        }
-
-        /**
-         * Set languages directory.
-         */
-        public function load_textdomain() {
-            load_plugin_textdomain( 'prevent-browser-caching', false, dirname(plugin_basename(__FILE__)) . '/lang/' );
-        }
-
-        /**
-         * Sanitize and return the options in the right form.
-         * @param $options
-         * @return array
-         */
-        public function filter_options( $options ) {
-            if ( isset( $options['clear_cache_automatically'] ) ) {
-                $clear_cache_automatically = esc_html( sanitize_text_field( $options['clear_cache_automatically'] ) );
-
-                if ( ! in_array( $clear_cache_automatically, array( 'every_time', 'every_period', 'never' ) ) ) {
-                    $clear_cache_automatically = 'every_time';
-                }
-            } else {
-                $clear_cache_automatically = 'every_time';
-            }
-
-            if ( isset( $options['clear_cache_automatically_minutes'] ) ) {
-                $clear_cache_automatically_minutes = intval( $options['clear_cache_automatically_minutes'] );
-                $clear_cache_automatically_minutes = min( $clear_cache_automatically_minutes, 99999 );
-                $clear_cache_automatically_minutes = max( $clear_cache_automatically_minutes, 1 );
-            } else {
-                $clear_cache_automatically_minutes = 10;
-            }
-
-            return array(
-                'clear_cache_automatically' => $clear_cache_automatically,
-                'clear_cache_automatically_minutes' => $clear_cache_automatically_minutes
+if ( ! function_exists( 'prevent_browser_caching' ) ) {
+    function prevent_browser_caching( $args = array() ) {
+        if ( ! class_exists('Prevent_Browser_Caching_Function') ) {
+            $default_args = array(
+                'assets_version' => time()
             );
-        }
 
-        /**
-         * Get value of prevent_browser_caching_options option.
-         */
-        public function get_options() {
-            if ( empty( $this->options ) ) {
-                $this->options = $this->filter_options( get_option('prevent_browser_caching_options') );
+            $assets_version = isset( $args['assets_version'] ) ? $args['assets_version'] : $default_args['assets_version'];
+
+            class Prevent_Browser_Caching_Function {
+                public $assets_version = '';
+
+                /**
+                 * Prevent_Browser_Caching_Function constructor.
+                 * @param $assets_version
+                 */
+                public function __construct( $assets_version ) {
+                    $this->assets_version = $assets_version;
+
+                    add_filter( 'style_loader_src', array( $this, 'add_query_arg' ), 10000 );
+                    add_filter( 'script_loader_src', array( $this, 'add_query_arg' ), 10000 );
+                }
+
+                /**
+                 * Adds query parameters to CSS and JS files.
+                 * @param $src
+                 * @return string
+                 */
+                public function add_query_arg( $src )
+                {
+                    if ( $this->assets_version ) {
+                        $src = add_query_arg( 'ver', $this->assets_version, $src );
+                    } else {
+                        $src = remove_query_arg( 'ver', $src );
+                    }
+
+                    return $src;
+                }
+
             }
 
-            return $this->options;
+            new Prevent_Browser_Caching_Function( $assets_version );
         }
+    }
+}
 
-        /**
-         * Get values of prevent_browser_caching_clear_cache_time option.
-         */
-        public function get_clear_cache_time() {
-            if ( ! $this->clear_cache_time ) {
-                $this->clear_cache_time = intval( get_option('prevent_browser_caching_clear_cache_time') );
-            }
+function maybe_load_class_prevent_browser_caching() {
+    if ( ! class_exists('Prevent_Browser_Caching') && ! class_exists('Prevent_Browser_Caching_Function') ) {
 
-            return $this->clear_cache_time;
-        }
-
-        /**
-         * Add query parameters to CSS and JS files.
-         * @param $src
-         * @return string
-         */
-        public function add_query_arg( $src ) {
-            if ( $time = $this->time_query_arg ) {
-                $src = add_query_arg( 'time', $time, $src );
-            }
-
-            return $src;
-        }
+        include_once 'includes/class-prevent-browser-caching.php';
 
     }
-
-    Prevent_Browser_Caching::instance();
-
 }
+add_action( 'after_setup_theme', 'maybe_load_class_prevent_browser_caching' );
--- readme.txt
+++ readme.txt
@@ -1,21 +1,35 @@
 === Prevent Browser Caching ===
-Tags: prevent, browser cache, javascript, css, js, scripts, styles, new version, frontend, development
+Tags: browser caching, browser cache, update css, update js, assets, frontend, development, browser, client, develop,
 Requires at least: 4.0
-Tested up to: 4.9.1
-Stable tag: 2.0
+Tested up to: 4.9.6
+Stable tag: 2.2
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Updates the version of all CSS and JS files.
+Update the version of all CSS and JS files. Show the latest changes on the site without asking the client to clear browser cache.
 
 == Description ==
 
 Are you a frontend developer? Do you want to clear browser cache for all users?
 Just activate this plugin and show your work!
 
-Prevent Browser Caching provides you with options to update the version of all CSS and JavaScript files automatically.
+Prevent Browser Caching allows you to update the version of all CSS and JS files automatically or manually in one click.
 
-You can also update styles and scripts for all site visitors manually in one click.
+Now you can show the latest changes on the site without asking the client to clear the cache.
+
+= How it works? =
+
+By default, WordPress loads assets using query param "ver" in the URL (e.g., style.css?ver=4.9.6). It allows browsers to cache these files until the parameter will not be updated.
+
+To prevent caching of CSS and JS files, this plugin adds a unique number (e.g., 1526905286) to the "ver" parameter (e.g., style.css?ver=4.9.6.1526905286) for all links, loaded using wp_enqueue_style and wp_enqueue_script functions.
+
+= For developers =
+
+You can set the version of CSS and JS files programmatically and disable admin functionality of this plugin.
+
+Just insert "prevent_browser_caching( array( 'assets_version' => '123' ) );" in functions.php file of your theme.
+
+Please let me know if it is useful for you and if you need additional options.
 
 == Installation ==
 
@@ -27,11 +41,18 @@ You can also update styles and scripts for all site visitors manually in one cli
 
 = From WordPress.org site =
 
-1. Download Prevent Browser Caching.
+1. Download Prevent Browser Caching plugin.
 2. Upload the "prevent-browser-caching" directory to your "/wp-content/plugins/" directory.
 3. Activate Prevent Browser Caching on your Plugins page.
 
 == Changelog ==
+
+= 2.2 =
+* Added function "prevent_browser_caching" which disables all admin settings of this plugin and allows to set the new settings.
+* Changing "ver" param instead of adding additional "time" param.
+
+= 2.1 =
+* Added option to show "Update CSS/JS" button on the toolbar.
 
 = 2.0 =
 * Added setting page to the admin panel.
